3b11f5f20df8664a7cc6d12e72c748b5b6e6f770
Christian Muñoz <chgeovany.15@gmail.com>
2021-08-25 16:01:54 -0500

    Impresion de variables

--- HolaMundo.c
+++ HolaMundo.c
@@ -7,9 +7,16 @@ int main()
     int integerA = 1;
     float floatA = 1.1;
     char letterA = 'A';
+    double doubleA =  1.2;  //Es un decimal al igual que un float pero dispone de mas valor en memoria para guardar valores
     bool boolean = false;
 
-    printf("Hello world!\n");
+    //En la siguiente linea estamos imprimiendo el integerA
+    printf("El valor de nuestro entero A es: %i\n\n",integerA);
     
+    printf("El valor de mi floatante A es: %f\n\n",floatA);
+
+    printf("El valor de mi Double A es: %f\n\n",doubleA);
+
+    printf("EL valor de mi caracter A es: %c\n\n",letterA);
     return 0;
 }
