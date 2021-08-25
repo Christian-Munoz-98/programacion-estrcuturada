3a3dcada25cb18054e075d3c25c8f1bb0727d096
Christian Muñoz <chgeovany.15@gmail.com>
2021-08-25 15:46:04 -0500

    Hellow world

---
+++ HolaMundo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int integerA = 1;
+    float floatA = 1.1;
+    char letterA = 'A';
+    bool boolean = false;
+
+    printf("Hello world!\n");
+    
+    return 0;
+}
